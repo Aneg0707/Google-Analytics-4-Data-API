cf7a2f09eb837181fdda44a4f22abae2189f7a31
Hennadii <106271954+Aneg0707@users.noreply.github.com>
2024-06-22 21:28:29 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+# Path to your service account key file
+KEY_FILE_LOCATION = r'C:\Users\YOUR CREDENTIAL GOOGLE CONSOLE FILE LOCATION' #YOUR CREDENTIAL GOOGLE CONSOLE FILE LOCATION
+
+# Google Analytics property ID
+PROPERTY_ID = '*************' #YOUR ID
